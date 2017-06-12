b4effb99922743f90cd379b95fffd6ef4ede17b4
Jorge <jasmad@hotmail.es>
2017-06-12 17:12:07 +0200

    Select

--- services/phonelist.service.js
+++ services/phonelist.service.js
@@ -1,6 +1,8 @@
-
+var config = require('config.json');
+var mongo = require('mongoskin');
 var db = mongo.db(config.connectionString, { native_parser: true });
-db.bind(process.env.PHONE_LIST);
+//db.bind(process.env.PHONE_LIST);
+db.bind('listin');
 var Q = require('q');
 var service = {};
 
@@ -13,11 +15,12 @@ module.exports = service;
 function findOne(phoneNumber)
 {
 	 var deferred = Q.defer();
-	 db.logs.findOne(
-			 {number:phoneNumer}, 
+	 db.listin.findOne({NVOMSISDN:phoneNumber}, 
 			 function(err,doc)
 			 {
 				 if (err) deferred.reject(err.name + ': ' + err.message);
+				 
+				 deferred.resolve();
 			 });
 	 return deferred.promise;
 }
