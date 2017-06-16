eba5d81d6a827a0fb63d53a352ef3b0b418a5f5d
angelperea <aperea.z29@gmail.com>
2017-06-16 13:10:16 +0200

    Consulta de usuarios

--- app/home/index.controller.js
+++ app/home/index.controller.js
@@ -23,18 +23,67 @@
         }
         
         function findNumber() {
-        	 console.log ('a ' + vm.listin.NVOMSISDN);
-             PhoneListService.findNumber(vm.listin)
-                .then(function (data) {
-                    //FlashService.Success(data.TIPO);
-                    resultSearchProcess(data.TIPO, vm.listin.NVOMSISDN);
-                    //console.log ('ddd: ' + data.TIPO);
-                })
-                .catch(function (error) {
-                    FlashService.Error(error);
-                });
-                
+        	 //console.log ('a ' + vm.listin.NVOMSISDN);
+        	 initPanels();
+        	 
+        	 if (vm.listin.NVOMSISDN != undefined) {
+        		 showSpinner('block');
+                 PhoneListService.findNumber(vm.listin)
+                 .then(function (data) {
+                     //FlashService.Success(data.TIPO);
+                     resultSearchProcess(data.TIPO, vm.listin.NVOMSISDN);
+                     //console.log ('ddd: ' + data.TIPO);
+                 })
+                 .catch(function (error) {
+                     FlashService.Error(error);
+                 });        		 
+        	 } else {
+        		 FlashService.Error('Debes rellenar el número a búscar');
+        	 }
         }
+        function showSpinner(state){
+        	$('#spinner').css('display',state);
+        }
+        
+        /* Funciones estandar */
+    	function initPanels() {
+    		//Inicializamos los paneles. Lo normal es que sea al entrar en el propio campo y como minimo al pulsar sobre el boton de busqueda
+    		/*
+    		$('#response-empty').css('display','none');
+    		$('#response-a').css('display','none');
+    		$('#response-g').css('display','none');
+    		$('#messageMobile-panel').css('display','none');
+    		*/
+    		$('#response-empty').hide(1000);
+    		$('#response-a').hide(1000);
+    		$('#response-g').hide(1000);
+    		$('#messageMobile-panel').css('display','none');
+    		//El spinner tambien por si se hubiese quedado por ahi
+    		showSpinner('none');
+    	}
+    	function resultSearchProcess(resultado, phoneNumber) {
+    		//Mostramos los paneles segun la respuest aque debuelva la consulta
+    		$('#spinner').css('display','none');
+    		if (resultado === "A") {
+    			$('#response-empty').css('display','block');
+    			$('#response-empty-phoneSearch').html(phoneNumber);
+    		} else if (resultado === "B") {
+    			$('#response-a').css('display','block');
+    			$('#response-a-phoneSearch').html(phoneNumber);
+    		} else if (resultado === null || resultado === undefined) {
+    			$('#response-g').css('display','block');
+    			$('#response-g-phoneSearch').html(phoneNumber);
+    		}
+    		$('#messageMobile-panel').css('display','block');	
+    	}
+    	$(document).ready(function(){
+    		//Inicializamos los paneles cuando vuelve a entrar en el campo. Damos por hecho que quiere una nueva consulta
+        	$("#phoneValidate").focusin(function(){
+            	initPanels();
+        	});
+        	//console.log(vm.user);
+    	});        
+        
     }
 
 })();
