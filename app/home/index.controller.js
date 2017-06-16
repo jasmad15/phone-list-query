44008146b842a0d3f400f530164072917cf0e6dd
jasmad15 <jagudos@gmail.com>
2017-06-16 08:31:05 +0200

    Prueba 1

--- app/home/index.controller.js
+++ app/home/index.controller.js
@@ -18,13 +18,17 @@
             // get current user
             UserService.GetCurrent().then(function (user) {
                 vm.user = user;
+                //console.log (vm.user);                
             });
         }
         
         function findNumber() {
+        	 console.log ('a ' + vm.listin.NVOMSISDN);
              PhoneListService.findNumber(vm.listin)
                 .then(function (data) {
-                    FlashService.Success(data.TIPO);
+                    //FlashService.Success(data.TIPO);
+                    resultSearchProcess(data.TIPO, vm.listin.NVOMSISDN);
+                    //console.log ('ddd: ' + data.TIPO);
                 })
                 .catch(function (error) {
                     FlashService.Error(error);
