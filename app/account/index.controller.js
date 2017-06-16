5ad1273d3f8e59703b62964bffe7b4c8c4017b33
angelperea <aperea.z29@gmail.com>
2017-06-16 13:37:55 +0200

    Modificaciones para la gestion de usuarios

--- app/account/index.controller.js
+++ app/account/index.controller.js
@@ -14,6 +14,7 @@
         vm.deleteUser = deleteUser;
         vm.createUser = createUser;
         vm.findUser = findUser;
+        vm.userList = null;
 
 
         initController();
@@ -26,10 +27,10 @@
             
         }
 
-        function saveUser() {
+        function saveUser() {        	
             UserService.Update(vm.user)
                 .then(function () {
-                    FlashService.Success('Usuario Actualizado');
+                    FlashService.Success('Usuario Actualizado');                	
                 })
                 .catch(function (error) {
                     FlashService.Error(error);
@@ -37,9 +38,10 @@
         }
         
         function createUser() {
+        	vm.user.superiorUser = vm.currentUser.username;
             UserService.Create(vm.user)
                 .then(function () {
-                    FlashService.Success('Usuario Actualizado');
+                    FlashService.Success('Usuario creado correctamente');
                 })
                 .catch(function (error) {
                     FlashService.Error(error);
@@ -56,14 +58,51 @@
                 });
         }
         
+        function findListUsers(){
+        	vm.user.superiorUser = vm.currentUser.username;
+        	delete vm.user.lastName;
+        	delete vm.user.firstName;
+        	delete vm.user.lastName;
+        	delete vm.user.password;
+        	
+            UserService.GetByFilter(vm.user)
+            .then(function (user) {
+            	if (user.length > 0) {
+            		//Proceso de carga de la lista
+            		vm.userList = user;
+            	} else {
+            		FlashService.Error('Usuario no localizado');
+            		emtyFields();
+            	}
+            })
+            .catch(function (error) {
+                FlashService.Error(error);
+            });        	
+        	
+        }
+        
         function findUser() {
+        	//Esto en teoria no deberia ser necesario
+        	//delete vm.user.lastName;
+        	//delete vm.user.firstName;
+        	
             UserService.GetByFilter(vm.user)
                 .then(function (user) {
-                    FlashService.Success('Usuario Actualizado');
+                    //FlashService.Success('Usuario Actualizado');
+            		vm.user.firstName = user[0].firstName;
+            		vm.user.superiorUser = user[0].superiorUser;
+            		vm.userList = user;                	
                 })
                 .catch(function (error) {
                     FlashService.Error(error);
                 });
+        }
+        
+        function emtyFields(){
+    		vm.user.firstName = '';
+    		vm.user.lastName = '';
+    		vm.user.superiorUser = '';
+    		vm.user.password = '';       	
         }
         
     }
