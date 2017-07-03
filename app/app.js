cda0fb86956c2699c48473d9e681d67bf295f59d
jasmad15 <jagudos@gmail.com>
2017-07-03 13:29:06 +0200

    Añadido ir pantalla login cuando caduca sesión

--- app/app.js
+++ app/app.js
@@ -4,12 +4,29 @@
     angular
         .module('app', ['ui.router'])
         .config(config)
-        .run(run);
+        .run(run)
+        .factory('authHttpResponseInterceptor',['$q','$location','$window', function($q,$location,$window){
+	return {
+		response: function(response){
+			if (response.status === 401) {
+				console.log("Response 401");
+			}
+			return response || $q.when(response);
+		},
+		responseError: function(rejection) {
+			if (rejection.status === 401) {
+				console.log("Response Error 401",rejection);
+				$window.location.href = '/login';
+			}
+			//return $q.reject(rejection);
+		}
+	}
+}]);
 
-    function config($stateProvider, $urlRouterProvider) {
+    function config($stateProvider, $urlRouterProvider, $httpProvider) {
         // default route
         $urlRouterProvider.otherwise("/");
-
+        $httpProvider.interceptors.push('authHttpResponseInterceptor');
         $stateProvider
             .state('home', {
                 url: '/',
@@ -25,6 +42,10 @@
                 controllerAs: 'vm',
                 data: { activeTab: 'account' }
             });
+        
+        
+       
+      
     }
 
     function run($http, $rootScope, $window) {
