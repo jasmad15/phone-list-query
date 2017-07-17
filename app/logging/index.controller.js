a979c7cb3ff56373185678e9a97b088ee7540f1c
jasmad15 <jagudos@gmail.com>
2017-07-17 14:20:25 +0200

    Añadido gestión de trazas

---
+++ app/logging/index.controller.js
@@ -0,0 +1,45 @@
+﻿
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('Logging.IndexController', Controller);
+
+    function Controller(UserService, FlashService, LoggerService) {
+        var vm = this;
+        vm.log = null;
+        
+        vm.getLogging = getLogging;
+        vm.getCSVLog = getCSVLog;
+       
+        
+        function getLogging() {
+       	 
+       		LoggerService.getLogging(vm.log)
+                .then(function (data) {
+                	//Mostramos los resultados
+                	console.log(data);
+                })
+                .catch(function (error) {
+                    FlashService.Error(error);
+                });        		 
+       }
+        
+        function getCSVLog() {
+       
+        	LoggerService.getCSVLog(vm.log)
+                .then(function (data) {
+                	//Descarga
+                	window.open(data);
+                	console.log(data);
+
+                })
+                .catch(function (error) {
+                    FlashService.Error(error);
+                });        		 
+       
+       }
+    }
+
+})();
